8fb9a3de7f46f3664cf12c50533d25658fda5186
Zmeegolov <karantinz@mail.ru>
2023-03-29 18:11:56 +0300

    Добавьте файлы проекта.

---
+++ junior1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace junior1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            string name = "Danila";
+            char gender = 'M';
+            bool isStudent = true;
+            double weight = 50.5;
+            float height = 60.1f;
+            long salary = 1234567890L;
+            decimal decimalValue = 110.10m;
+            byte numberOfBooks = 10;
+            short currentBalance = 10000;
+        }
+    }
+}
